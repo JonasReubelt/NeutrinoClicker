024fa5020bb4076491311050cd5857eceaa7f731
Tamas Gal <tgal@km3net.de>
2018-04-23 14:11:41 +0200

    Cleanup

--- src/app.js
+++ src/app.js
@@ -1,24 +1,3 @@
-console.log("App.js is running!");
-
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the handas of a computer',
-    options: ['One', 'Two']
-};
-
-
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
-
 let count = 0;
 const addOne = () => {
     count += 1;
@@ -32,7 +11,7 @@ const appRoot = document.getElementById('app');
 const renderApp = () => {
     const templateTwo = (
         <div>
-            <h1>Count: {count}</h1>
+            <h1>Neutrinos: {count}</h1>
             <button onClick={subtractOne}>-1</button>
             <button onClick={addOne}>+1</button>
             <button onClick={reset}>reset</button>
